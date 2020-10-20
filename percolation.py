ef46b19ce733da117414970474a8e0a1699091ed
Bone5505 <adamshaw5505@gmail.com>
2020-10-20 15:28:43 +0100

    added basic percolation? and step funcitonality

--- percolation.py
+++ percolation.py
@@ -8,6 +8,7 @@ __license__ = "MIT"
 import numpy as np
 import pygame
 import pygame_gui
+from functools import reduce
 
 
 class Percolation:
@@ -18,9 +19,11 @@ class Percolation:
         self.grid_size = (self.grid_width, self.grid_height) = (400, 400)
         self.max_array_length = self.grid_width*self.grid_height 
         self.sites = np.array([])
+        self.step_func = self.__SquarePercStep
+        self.draw_call = False
         self.p_slider = pygame_gui.elements.UIHorizontalSlider(
             pygame.Rect((10, 670), (600, 20)),
-            0.0,
+            0.25,
             (0.0, 1.0),
             gui_manager,
         )
@@ -35,9 +38,11 @@ class Percolation:
         )
 
     def Draw(self, window_surf):
-        self.draw_surface.fill((255,255,255))
-        for index in self.grid:
-            self.draw_surface.set_at((index%self.grid_width,index//self.grid_width),(0,0,0))
+        if self.draw_call:
+            self.draw_surface.fill((255,255,255))
+            for index in self.grid:
+                self.draw_surface.set_at((index%self.grid_width,index//self.grid_width),(0,0,0))
+            self.draw_call = False
         window_surf.blit(
             pygame.transform.scale(self.draw_surface, (600, 600)), (10, 60)
         )
@@ -47,9 +52,14 @@ class Percolation:
         window_surf.blit(img, (300 - img.get_rect().width // 2, 690))
 
     def GenRandomStart(self):
-        ran = np.arange(0,self.max_array_length)
-        np.random.shuffle(ran)
-        self.grid = ran[0:self.max_array_length//4]
+        self.grid = np.where(np.random.rand(self.max_array_length) < self.p_slider.current_value)[0]
+        self.draw_call = True
+
+    def __SquarePercStep(self):
+        self.grid = np.where(np.random.rand(self.max_array_length) < self.p_slider.current_value)[0]
+        self.draw_call = True
+
+
 
 # Create pygame window and gui manager
 pygame.init()
@@ -58,22 +68,42 @@ window_size = (window_width, window_height) = (720, 720)
 window_surface = pygame.display.set_mode(window_size)
 gui_manager = pygame_gui.UIManager(window_size)
 
+button_step = pygame_gui.elements.UIButton(pygame.Rect(620,60,90,50),"Step",gui_manager)
+button_play = pygame_gui.elements.UIButton(pygame.Rect(620,120,90,50),"Play",gui_manager)
+
 perc_manager = Percolation(gui_manager, window_size)
 perc_manager.GenRandomStart()
 
 is_running = True
+is_playing = False
 time_start = 0
 time_end = 0.01
 time_delta = 0.01
 
+time_timer = 0.0
+
 while is_running:
     time_end = pygame.time.get_ticks()
-    time_delta = time_end - time_start
+    time_delta = (time_end - time_start)/1000.0
     time_start = time_end
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == button_step:
+                    perc_manager.step_func()
+                elif event.ui_element == button_play:
+                    is_playing = not is_playing
+                    if is_playing: button_play.set_text("Pause")
+                    else: button_play.set_text("Play")
+        
         gui_manager.process_events(event)
+
+    time_timer += time_delta
+    if time_timer > 0.5:
+        time_timer -= 0.5
+        if is_playing: perc_manager.step_func()
 
     gui_manager.update(time_delta)
 
