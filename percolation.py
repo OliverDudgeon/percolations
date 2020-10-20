27cfc5211733732a00628602863c444df73a69d9
Bone5505 <adamshaw5505@gmail.com>
2020-10-20 11:33:32 +0100

    added python headers

---
+++ percolation.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+"""percolation.py: Monte Carlo Simulation of Percolation"""
+
+__author__ = "Oliver Dudgeon, Adam Shaw, Joseph Parker"
+__license__ = "MIT"
+
+import numpy
+import pygame
+import pygame_gui
