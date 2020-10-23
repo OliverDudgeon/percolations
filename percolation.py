b54c42d82e4ab6a0545f3aa8e9da00a4fa42671a
Oliver Dudgeon <oliverdudgeon@gmail.com>
2020-10-23 17:25:43 +0100

    Make percolation class inherit from a base class

--- percolation.py
+++ percolation.py
@@ -10,13 +10,46 @@ import pygame
 import pygame_gui
 import matplotlib.pyplot as plt
 
-class Percolation:  # Class to handle percolation things
+
+class BasePercolation:
+    """Base class for a percolation simulation"""
+
+    def __init__(
+        self, *, name: str, grid_size: int, gui_man: pygame_gui.UIManager
+    ) -> None:
+        """
+        name: the name of the simulation in the dropdown
+        grid_size: the number of cells on a row/column (the lattice is square)
+        gui_man: pygame gui manager to handle user events
+        """
+        self.name = name
+        self.grid_size = (self.grid_width, self.grid_height) = (
+            grid_size,
+            grid_size,
+        )
+
+        self.grid = np.zeros(
+            self.grid_width * self.grid_height
+        )  # Grid values, 1 = active, 0 = deaded
+
+    def draw(self):
+        pass
+
+    def process_events(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Percolation(BasePercolation):
+    """Class to handle percolation things."""
+
     def __init__(self, gui_man):
-        self.name = "Site Percolation" # Name to store in dropdown menu
-        self.grid_size = (self.grid_width, self.grid_height) = (100, 100) # Hardcoded grid size
-        self.grid = np.zeros(self.grid_width * self.grid_height) # Grid values, 1 = active, 0 = deaded
-        self.cluster = np.zeros_like(self.grid) # Init cluster values
-        self.draw_call = False # Used to optimize draw calls
+        super().__init__(name="Site Percolation", grid_size=100, gui_man=gui_man)
+
+        self.cluster = np.zeros_like(self.grid)  # Init cluster values
+        self.draw_call = False  # Used to optimize draw calls
         # Add UI elements for this percolation
         self.p_slider = pygame_gui.elements.UIHorizontalSlider(
             pygame.Rect((10, 670), (600, 20)),
@@ -30,23 +63,26 @@ class Percolation:  # Class to handle percolation things
         self.button_path = pygame_gui.elements.UIButton(
             pygame.Rect(620, 120, 90, 50), "Cluster", gui_man
         )
-
         self.button_graph = pygame_gui.elements.UIButton(
             pygame.Rect(620, 180, 90, 50), "Plot", gui_man
         )
-        self.draw_surface = pygame.Surface(self.grid_size) # Create surface to draw onto, for optimisation
-        self.font = pygame.font.SysFont(None, 25) # Init font for drawing
-        self.step() # Fill grid when initialising
+        self.draw_surface = pygame.Surface(
+            self.grid_size
+        )  # Create surface to draw onto, for optimisation
+        self.font = pygame.font.SysFont(None, 25)  # Init font for drawing
+        self.step()  # Fill grid when initialising
 
     def draw(self, window_surf):
-        if self.draw_call: # Only redraw grid if it has changed
-            self.draw_surface.fill((255, 255, 255)) # Clear old grid
-            for i in range(self.grid.size): #  Loop through all active sites and fill pixel black
+        if self.draw_call:  # Only redraw grid if it has changed
+            self.draw_surface.fill((255, 255, 255))  # Clear old grid
+            for i in range(
+                self.grid.size
+            ):  #  Loop through all active sites and fill pixel black
                 if self.grid[i] == 1:
                     self.draw_surface.set_at(
                         (i % self.grid_width, i // self.grid_height), (0, 0, 0)
                     )
-            self.draw_call = False # No need to redraw now
+            self.draw_call = False  # No need to redraw now
         # Copy surface to main window
         window_surf.blit(
             pygame.transform.scale(self.draw_surface, (600, 600)), (10, 60)
@@ -87,10 +123,10 @@ class Percolation:  # Class to handle percolation things
 
         for i in range(self.grid.size):
             if self.grid[i]:  # Loops through all the active sites
-                top = (
+                top = int(
                     0 if i % self.grid_width == 0 else self.cluster[i - 1]
                 )  # Cluster number of left site
-                left = (
+                left = int(
                     0
                     if i // self.grid_width == 0
                     else self.cluster[i - self.grid_width]
@@ -139,72 +175,108 @@ class Percolation:  # Class to handle percolation things
         N = self.grid.size
         # Plot results from many simulations
         plt.plot(ps, cs)  # Plot the number of cluster vs the probability
-        # A001168 - Number of fixed polyominoes with n cells. 
-        coeffs = [1, 2, 6, 19, 63, 216, 760, 2725, 9910, 36446, 135268, 505861, 1903890, 7204874, 27394666, 104592937, 400795844, 1540820542, 5940738676, 22964779660, 88983512783, 345532572678, 1344372335524, 5239988770268, 20457802016011, 79992676367108]
-        n_anyl = N * (sum([coeffs[i]*(ps**(i+1)) * (1-ps)**(i+2+2*round((i+1)**0.5)) for i in range(len(coeffs))]))
-        plt.plot(ps, n_anyl, c = 'r')
+        # A001168 - Number of fixed polyominoes with n cells.
+        coeffs = [
+            1,
+            2,
+            6,
+            19,
+            63,
+            216,
+            760,
+            2725,
+            9910,
+            36446,
+            135268,
+            505861,
+            1903890,
+            7204874,
+            27394666,
+            104592937,
+            400795844,
+            1540820542,
+            5940738676,
+            22964779660,
+            88983512783,
+            345532572678,
+            1344372335524,
+            5239988770268,
+            20457802016011,
+            79992676367108,
+        ]
+        n_anyl = N * (
+            sum(
+                [
+                    coeffs[i]
+                    * (ps ** (i + 1))
+                    * (1 - ps) ** (i + 2 + 2 * round((i + 1) ** 0.5))
+                    for i in range(len(coeffs))
+                ]
+            )
+        )
+        plt.plot(ps, n_anyl, c="r")
         plt.xlabel("Site Probability")
         plt.ylabel("Cluster Size")
         plt.show()
 
 
-# Create pygame window and gui manager
-pygame.init()
-pygame.display.set_caption("Percolation Simulation")
-window_size = (window_width, window_height) = (720, 720)
-window_surface = pygame.display.set_mode(window_size)
+if __name__ == "__main__":
+    # Create pygame window and gui manager
+    pygame.init()
+    pygame.display.set_caption("Percolation Simulation")
+    window_size = (window_width, window_height) = (720, 720)
+    window_surface = pygame.display.set_mode(window_size)
 
+    gui_manager = pygame_gui.UIManager(window_size)  # Initialize the GUI Manager
 
-gui_manager = pygame_gui.UIManager(window_size)  # Initialize the GUI Manager
+    percolation_list = []  # List containing all the percolators that we make
+    percolation_list.append(Percolation(gui_manager))
 
-percolation_list = []  # List containing all the percolators that we make
-percolation_list.append(Percolation(gui_manager))
-
-perc_selector = (
-    pygame_gui.elements.UIDropDownMenu(  # Drop down menu to select percolator
-        [p.name for p in percolation_list],
-        percolation_list[0].name,
-        pygame.Rect(10, 10, 600, 30),
-        gui_manager,
+    perc_selector = (
+        pygame_gui.elements.UIDropDownMenu(  # Drop down menu to select percolator
+            [p.name for p in percolation_list],
+            percolation_list[0].name,
+            pygame.Rect(10, 10, 600, 30),
+            gui_manager,
+        )
     )
-)
-current_perc = (perc_selector.options_list).index(
-    perc_selector.selected_option
-)  # Hold index of current percolator
+    current_perc = (perc_selector.options_list).index(
+        perc_selector.selected_option
+    )  # Hold index of current percolator
 
-# Time keeping variables + misc.
-is_running = True
-is_playing = False
-time_start = 0
-time_end = 0.01
-time_delta = 0.01
+    # Time keeping variables + misc.
+    is_running = True
+    is_playing = False
+    time_start = 0
+    time_end = 0.01
+    time_delta = 0.01
 
-time_timer = 0.0
+    time_timer = 0.0
 
-while is_running:  # Main loop for GUI
-    # Calculates delta time
-    time_end = pygame.time.get_ticks()
-    time_delta = (time_end - time_start) / 1000.0
-    time_start = time_end
-    for event in pygame.event.get():  # Main event loop for pygame
-        if event.type == pygame.QUIT:  # Handle QUIT event
-            is_running = False
-        if event.type == pygame.USEREVENT:
-            if (
-                event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED
-            ):  # Changes the current percolator if drop down menu is changed
-                current_perc = (perc_selector.options_list).index(
-                    perc_selector.selected_option
-                )
-        percolation_list[current_perc].process_events(event)
-        gui_manager.process_events(event)  # Update the events for gui
+    while is_running:  # Main loop for GUI
+        # Calculates delta time
+        time_end = pygame.time.get_ticks()
+        time_delta = (time_end - time_start) / 1000.0
+        time_start = time_end
+        for event in pygame.event.get():  # Main event loop for pygame
+            if event.type == pygame.QUIT:  # Handle QUIT event
+                is_running = False
+            if event.type == pygame.USEREVENT:
+                if (
+                    event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED
+                ):  # Changes the current percolator if drop down menu is changed
+                    current_perc = (perc_selector.options_list).index(
+                        perc_selector.selected_option
+                    )
+            percolation_list[current_perc].process_events(event)
+            gui_manager.process_events(event)  # Update the events for gui
 
-    # Update GUI with delta time
-    gui_manager.update(time_delta)
-    percolation_list[current_perc].update(time_delta)
+        # Update GUI with delta time
+        gui_manager.update(time_delta)
+        percolation_list[current_perc].update(time_delta)
 
-    # Draw all the GUI
-    window_surface.fill((44, 47, 51))
-    percolation_list[current_perc].draw(window_surface)
-    gui_manager.draw_ui(window_surface)
-    pygame.display.update()
+        # Draw all the GUI
+        window_surface.fill((44, 47, 51))
+        percolation_list[current_perc].draw(window_surface)
+        gui_manager.draw_ui(window_surface)
+        pygame.display.update()
