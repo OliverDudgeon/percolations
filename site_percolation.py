0613310712fca50f9332442a240ade7f2dcbab78
Oliver Dudgeon <oliverdudgeon@gmail.com>
2020-10-24 20:58:19 +0100

    Re-tidyup site percolation

--- site_percolation.py
+++ site_percolation.py
@@ -7,21 +7,21 @@ import numpy as np
 import pygame
 import pygame_gui
 from pygame_gui.elements import UIButton, UIHorizontalSlider
-import matplotlib.pyplot as plt
 
 from base_percolation import BasePercolation
-from coefficients import COEFFS
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BTN_WH = (90, 50)
+
+GRID_CONTAINER_SIZE = 600
 
 
 class SitePercolation(BasePercolation):
     """Class to handle percolation things."""
 
     def __init__(self):
-        super().__init__(name="Site Percolation", grid_size=100)
+        super().__init__(name="Site Percolation", grid_size=50)
 
         self.cluster = np.zeros_like(self.grid)  # Init cluster values
         self.draw_call = False  # Used to optimize draw calls
@@ -36,14 +36,7 @@ class SitePercolation(BasePercolation):
         self.button_step = None
         self.button_path = None
         self.button_graph = None
-
-    def step(self):
-        """Do one step of the simulation"""
-        self.cluster = np.zeros_like(self.grid, dtype=np.int)  # Reset cluster values
-
-        # Repopulate sites
-        self.grid = np.random.rand(self.grid.size) < self.p_slider.current_value
-        self.draw_call = True  # Call a redraw
+        self.draw_clusters = None
 
     def enable(self, gui_manager: pygame_gui.UIManager):
         """Create the UI elements"""
@@ -71,31 +64,39 @@ class SitePercolation(BasePercolation):
 
             #  Loop through all active sites and fill pixel black
             for i in range(self.grid.size):
+                row = i % self.grid_size
+                column = i // self.grid_size
                 if self.grid[i] == 1:
-                    self.draw_surface.set_at(
-                        (i % self.grid_size, i // self.grid_size), (0, 0, 0)
-                    )
+                    self.draw_surface.set_at((row, column), (0, 0, 0))
             self.draw_call = False  # No need to redraw now
+
         # Copy surface to main window
         window_surf.blit(
-            pygame.transform.scale(self.draw_surface, (600, 600)), (10, 60)
+            pygame.transform.scale(
+                self.draw_surface, (GRID_CONTAINER_SIZE, GRID_CONTAINER_SIZE)
+            ),
+            (10, 60),
         )
         # Copy surface to main window
-        window_surf.blit(pygame.transform.scale(surface, (600, 600)), (10, 60))
+        window_surf.blit(
+            pygame.transform.scale(surface, (GRID_CONTAINER_SIZE, GRID_CONTAINER_SIZE)),
+            (10, 60),
+        )
 
         if self.draw_clusters:
-            s = 600 // self.grid_size
+            site_size = GRID_CONTAINER_SIZE // self.grid_size
             for i in range(self.grid.size):
+                row = i % self.grid_size
+                column = i // self.grid_size
                 if not self.cluster[i]:
                     continue
                 img = self.font.render(str(self.cluster[i]), True, (255, 0, 0), None)
-                c = (10 + (i % self.grid_size) * s, 60 + (i // self.grid_size) * s)
+                cluster = (10 + row * site_size, 60 + column * site_size)
                 window_surf.blit(
-                    pygame.transform.smoothscale(
-                        img, (600 // self.grid_size, 600 // self.grid_size)
-                    ),
-                    c,
+                    pygame.transform.smoothscale(img, (site_size, site_size)),
+                    cluster,
                 )
+
                 # Transform index of 1D array to 2D array
                 pos = (i % self.grid_size, i // self.grid_size)
                 surface.set_at(pos, BLACK)
@@ -119,11 +120,12 @@ class SitePercolation(BasePercolation):
     def update(self, delta) -> None:
         return
 
-    def step(self):  # Do one step of the simulation
-        self.cluster = np.zeros_like(self.grid, np.int)  # Reset cluster values
-        self.grid = (
-            np.random.rand(self.grid.size) < self.p_slider.current_value
-        )  # Repopulate sites
+    def step(self):
+        """Do one step of the simulation"""
+        self.cluster = np.zeros_like(self.grid, dtype=np.int)  # Reset cluster values
+
+        # Repopulate sites
+        self.grid = np.random.rand(self.grid.size) < self.p_slider.current_value
         self.draw_call = True  # Call a redraw
         self.draw_clusters = False
 
@@ -139,41 +141,42 @@ class SitePercolation(BasePercolation):
             [0 for l in range(self.grid.size // 2)]
         )  # Links clusters together that we're not previously found to be together
 
-        def find(x):  # Loops through labels indexing them correctly
-            y = x
-            while (labels[y] != y).any():
-                y = labels[y]
+        def find(x_pos):  # Loops through labels indexing them correctly
+            y_pos = x_pos
+            while (labels[y_pos] != y_pos).any():
+                y_pos = labels[y_pos]
 
-            while (labels[x] != x).any():
-                z = labels[x]
-                labels[x] = y
-                x = z
-            return y
+            while (labels[x_pos] != x_pos).any():
+                label = labels[x_pos]
+                labels[x_pos] = y_pos
+                x_pos = label
+            return y_pos
 
-        def union(x, y):  # Links to cluster together
-            fy = find(y)
-            labels[find(x)] = fy
-            return fy
+        def union(x_pos, y_pos):  # Links to cluster together
+            found_y = find(y_pos)
+            labels[find(x_pos)] = found_y
+            return found_y
 
         for i in range(self.grid.size):
+            row = i % self.grid_size
+            column = i // self.grid_size
             if self.grid[i]:  # Loops through all the active sites
-                top = (
-                    0 if i % self.grid_size == 0 else self.cluster[i - 1]
-                )  # Cluster number of left site
-                left = (
-                    0 if i // self.grid_size == 0 else self.cluster[i - self.grid_size]
-                )  # Cluster number of top site
+                # Cluster number of left site
+                top = 0 if row == 0 else self.cluster[i - 1]
+                # Cluster number of top site
+                left = 0 if column == 0 else self.cluster[i - self.grid_size]
 
-                c = int(top > 0) + int(left > 0)  # Counts how many sites are next to it
-                if c == 0:  # If not in cluster, add to new one
+                cluster = int(top > 0) + int(
+                    left > 0
+                )  # Counts how many sites are next to it
+                if cluster == 0:  # If not in cluster, add to new one
                     labels[0] += 1
                     labels[labels[0]] = labels[0]
                     self.cluster[i] = labels[0]
-                elif (
-                    c == 1
-                ):  # Next to one cluster so set to the cluster number (one of left or top will be zero)
+                elif cluster == 1:
+                    # Next to one cluster so set to the cluster number (one of left or top will be zero)
                     self.cluster[i] = max(top, left)
-                elif c == 2:  # Next to two clusters
+                elif cluster == 2:  # Next to two clusters
                     self.cluster[i] = union(top, left)
         labels[0] = 0
         self.cluster = find(self.cluster)  # Finds all the unique labels for clusters
@@ -199,13 +202,14 @@ class SitePercolation(BasePercolation):
         return 0
 
     def simulate(self):
+        """"""
         # To find critical point WIP
         top = 0
-        bot = 0
+        bottom = 0
         for l in range(100):
             self.grid = (np.random.rand(self.grid.size) < 0.60).astype(np.int)
             top += self.hoshen_kopelman()
         for l in range(100):
             self.grid = (np.random.rand(self.grid.size) < 0.59).astype(np.int)
-            bot += self.hoshen_kopelman()
-        print((0.59 * bot + 0.60 * top) / (top + bot))
+            bottom += self.hoshen_kopelman()
+        print((0.59 * bottom + 0.60 * top) / (top + bottom))
