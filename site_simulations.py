4b0f08a46553ba1cdd12b516a3337b143d84c030
b0nagro <adamshaw5505@gmail.com>
2020-10-29 20:15:23 +0000

    Reworked simulation file to generate graphs

--- site_simulations.py
+++ site_simulations.py
@@ -2,102 +2,106 @@
 
 import numpy as np
 from multiprocessing import Pool
-from scipy.ndimage import measurements
+from scipy.ndimage import label
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 
-def _is_infinite(grid: np.ndarray):
-    size = grid.shape[0]
-    grid, _ = measurements.label(grid.reshape((size, size)))
-    grid = grid.flatten()
-
-    top_bot = np.intersect1d(
-        grid[:size],
-        grid[grid.size - size :],
-    )
-    # Find cluster numbers on left and right
-    left_right = np.intersect1d(
-        grid[size - 1 :: size],
-        grid[0::size],
-    )
-    # Remove empty sites
-    top_bot = top_bot[top_bot != 0]
-    left_right = left_right[left_right != 0]
-    # Returns 1 if a cluster belongs on opposite edges, 0 otherwise
-    return int(left_right.size > 0 or top_bot.size > 0)
-
-
-def _sim_critical_point():
-    """To find critical point WIP"""
-    p_low = 0.0
-    p_high = 1.0
-    while abs(p_high - p_low) > 0.0001:
-        p = (p_high + p_low) / 2
-        grid = (np.random.rand(40000, 40000) < p).astype(np.int)
-        is_infinite = _is_infinite(grid)
-        if is_infinite:
-            p_high = p
-        else:
-            p_low = p
-        print(p_low, p_high)
-    print(f"Critical Probability {(p_high+p_low)/2.0}")
-
-
-def _sim_cluster_numbers():
-    """To find number of clusters"""
-    prob_list = np.linspace(0.0, 1.0, 500)
-    numb_list = np.zeros_like(prob_list)
-    for i in range(prob_list.size):
-        print(i)
-        grid = (np.random.rand(5000, 5000) < prob_list[i]).astype(np.int)
-        _, n = measurements.label(grid)
-        numb_list[i] = n
-    plt.plot(prob_list, numb_list)
-    # plt.plot(prob_list[1:], np.diff(numb_list))
-    plt.show()
-
-
-def _label_dot_counts():
-    prob_list = np.linspace(0.0, 1.0, 500)
-    numb_list = np.zeros_like(prob_list)
+def _gamma_exponent():
+    N = 10000
+    pc = 0.59274
+    prob_list = np.linspace(0, 1.0, 100)
+    cluster_mean = np.zeros_like(prob_list)
     try:
-        numb_list = np.load("./site_data/ldc_data.npy")
+        cluster_mean = np.load("./site_data/clmean_data.npy")
     except FileNotFoundError:
         for i in range(prob_list.size):
             print(i)
-            grid = (np.random.rand(5000, 5000) < prob_list[i]).astype(np.int)
-            labels, _ = measurements.label(grid)
-            label, sizes = np.unique(labels, return_counts=True)
-            numb_list[i] = np.dot(label, sizes)
-        np.save("./site_data/ldc_data.npy", numb_list)
+            grid = np.random.rand(N, N) < prob_list[i]
+            _, s = label(grid)
+            cluster_mean[i] = np.sum(grid) / s
+        np.save("./site_data/clmean_data.npy", cluster_mean)
 
-    plt.plot(prob_list, numb_list)
-    plt.xlabel("Site Probability")
-    plt.ylabel("Sum of label numbers times cluster size")
-    try:
-        f = open("ldc_fig.svg")
-        f.close()
-    except FileNotFoundError:
-        plt.savefig("./site_data/ldc_fig.svg", format="svg")
-        plt.savefig("./site_data/ldc_fig.pdf", format="pdf")
+    # prob_list -= pc
+    plt.plot(prob_list, cluster_mean)
+    # plt.plot(np.log(prob_list), np.log(cluster_mean))
     plt.show()
 
 
-def _cluster_size_deviation():
-    prob_list = np.linspace(0.0, 1.0, 500)
-    numb_list = np.zer / os_like(prob_list)
-    for i in range(prob_list.size):
-        print(i)
-        grid = (np.random.rand(5000, 5000) < prob_list[i]).astype(np.int)
-        labels, _ = measurements.label(grid)
-        _, sizes = np.unique(labels, return_counts=True)
-        size, counts = np.unique(sizes, return_counts=True)
-        numb_list[i] = np.dot(size ** 2, counts)
-    plt.plot(prob_list, numb_list)
+def _fisher_exponent():
+    N = 35000
+    grid = np.random.rand(N, N) < 0.59274
+    labels, _ = label(grid)
+    _, size = np.unique(labels, return_counts=True)
+    size, counts = np.unique(size, return_counts=True)
+    size = size[counts > N // 4]
+    counts = counts[counts > N // 4] / np.sum(counts)
+
+    def func(s, m, c):
+        return m * s + c
+
+    popt, pcov = curve_fit(func, np.log(size), np.log(counts))
+    print(-popt[0])
+    plt.plot(np.log(size), np.log(counts))
+    plt.plot(np.log(size), func(np.log(size), *popt))
+    plt.show()
+
+
+def _beta_exponent():
+    size = 20000
+    pc = 0.59274
+    prob_list = np.linspace(0.59274, 0.7, 300)
+    frac_list = np.zeros_like(prob_list)
+    try:
+        frac_list = np.load("./site_data/order_data.npy")
+    except FileNotFoundError:
+        for i in range(prob_list.size):
+            print(i)
+            grid = np.random.rand(size, size) < prob_list[i]
+            labels, _ = label(grid)
+            nlabel, count = np.unique(labels, return_counts=True)
+            top_bot = np.intersect1d(labels[:][0], labels[:][size - 1])
+            left_right = np.intersect1d(labels[0][:], labels[size - 1][:])
+            for j in np.union1d(top_bot, left_right)[1:]:
+                frac_list[i] += count[np.where(nlabel == j)[0]] / (size * size)
+        np.save("./site_data/order_data.npy", frac_list)
+    prob_list -= pc
+    frac_list = frac_list[prob_list > 0]
+    prob_list = prob_list[prob_list > 0]
+
+    prob_list = prob_list[frac_list > 0]
+    frac_list = frac_list[frac_list > 0]
+    np.log(frac_list)
+
+    def func(x, m, c):
+        return m * x + c
+
+    popt, pact = curve_fit(func, np.log(prob_list), np.log(frac_list))
+
+    plt.plot(prob_list, frac_list)
+    plt.xlabel(r"|$p - p_{c}$|")
+    plt.ylabel(r"P")
+    plt.title(
+        r"Order parameter vs site probability for 2d site percolation, $P\sim|p-p_{c}|^{\beta}$"
+    )
+    plt.legend(["Data"])
+
+    plt.savefig("./site_data/order_fig.pdf", format="pdf")
+    plt.savefig("./site_data/order_fig.svg", format="svg")
+    plt.show()
+
+    plt.plot(np.log(prob_list), np.log(frac_list))
+    plt.plot(np.log(prob_list), func(np.log(prob_list), *popt))
+    plt.xlabel(r"$\log |p-p_{c}|$")
+    plt.ylabel(r"$\log P$")
+    plt.title(
+        f"Log-Log plot of order parameter vs site probability to find beta exponent $\\beta = {popt[0]}$"
+    )
+    plt.legend(["Data", "Linear Regression"])
     plt.show()
 
 
 if __name__ == "__main__":
-    # _sim_critical_point()
-    # _sim_cluster_numbers()
-    _label_dot_counts()
+    _gamma_exponent()
+    # _fisher_exponent()
+    # _beta_exponent()
