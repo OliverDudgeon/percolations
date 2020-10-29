313c57c2306df39e5378011ce4d3fc49e3eea8f5
Joseph Parker <67790622+JGWParker@users.noreply.github.com>
2020-10-29 18:43:34 +0000

    Added multiple plot options

--- forest_fire.py
+++ forest_fire.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 import numpy as np
 import pygame
 import pygame_gui
-from pygame_gui.elements import UILabel, UITextEntryLine, UIButton
+from pygame_gui.elements import UILabel, UITextEntryLine, UIButton, UIDropDownMenu
 import matplotlib.pyplot as plt
 
 from base_percolation import BasePercolation
@@ -26,12 +26,13 @@ class ForestFire(BasePercolation):
         self.timer = 0
         self.is_playing = False
         self.is_simulating = False
+        self.plot_option_mean = self.plot_option_raw = self.plot_option_order = False
         self.iterative_tree_frac = 0
         self.step_iteration = 0
-        self.probabilities = 30
-        self.sim_reps = 20
-        self.sim_times = np.zeros((1, self.probabilities))
-        self.full_sim_times = np.zeros((self.sim_reps, self.probabilities))
+        self.probabilities = 25 # Edit number of data points in plots
+        self.sim_reps = 20 # Edit the number of repeats
+        self.sim_times = np.zeros((1,self.probabilities))
+        self.full_sim_times = np.zeros((self.sim_reps,self.probabilities))
         self.draw_surface = pygame.Surface((self.grid_size, self.grid_size))
         self.font = pygame.font.SysFont(None, 25)
 
@@ -79,9 +80,10 @@ class ForestFire(BasePercolation):
         )
 
         self.button_play = UIButton(pygame.Rect(620, 120, 90, 50), "Play", gui_manager)
-        self.button_sim = UIButton(
-            pygame.Rect(620, 180, 90, 50), "Simulate", gui_manager
-        )
+        self.button_sim = UIButton(pygame.Rect(540, 700, 90, 50), "Simulate", gui_manager)
+        self.menu_plot = UIDropDownMenu(("Plot mean","Plot raw data","Plot order parameter"),"Plot mean",
+                                        pygame.Rect(490,750,190,20),gui_manager)
+                                         
         self.generate_random_start()
 
     def draw(self, window_surf: pygame.Surface):
@@ -130,46 +132,66 @@ class ForestFire(BasePercolation):
                     self.button_play.set_text("Play")
             if event.ui_element == self.button_sim:
                 self.is_playing = False
-                self.init_sim()
-
+                if self.menu_plot.selected_option == ("Plot mean"):
+                    self.plot_option_mean = True # Plot mean values
+                    self.plopt_option_raw = self.plot_option_order = False
+                    self.init_sim()
+                elif self.menu_plot.selected_option == ("Plot raw data"):
+                    self.plot_option_raw = True # Plot raw data
+                    self.plopt_option_mean = self.plot_option_order = False
+                    self.init_sim()
+                else:
+                    self.plot_option_order = True # Plot order parameter (also plots mean but much slower)
+                    self.plopt_option_mean = self.plot_option_raw = False
+                    self.full_order_parameter_array = np.zeros_like(self.full_sim_times)
+                    self.order_parameter_array = np.zeros_like(self.sim_times)
+                    self.init_sim()
+                
         elif event.user_type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
             self.is_playing = False
             self.button_play.set_text("Play")
             if self.validate_entries():
                 self.generate_random_start()
+                
 
-    # Initiate criticality simulation
     def init_sim(self):
-        # Repeat algorithm multiple times
-        for i1 in range(0, self.sim_reps):
+        """Produce plots of critical behaviour in a modified algorithm"""
+        # Repeat algorithm multiple times 
+        for i1 in range(0,self.sim_reps):
             # Perform calculation for multiple tree coverage fractions
-            for i2 in range(1, self.probabilities):
-                self.iterative_tree_frac = i2 / self.probabilities
+            for i2 in range(1,self.probabilities):
+                self.iterative_tree_frac = i2/self.probabilities
                 self.is_simulating = True
                 self.generate_random_start()
                 while self.is_simulating == True:
                     self.sim_step()
             print(self.sim_times)
-            self.full_sim_times[i1, :] = self.sim_times
-
-        # Calculate mean simulation times, save data and plot graph
-        sim_times_final = np.mean(self.full_sim_times, axis=0)
-        savestr = (
-            "data_"
-            + str(self.sim_reps)
-            + "reps_"
-            + str(self.probabilities)
-            + "probs.npy"
-        )
-        np.save(savestr, sim_times_final)
+            self.full_sim_times[i1,:] = self.sim_times
+            if self.plot_option_order == True:
+                self.full_order_parameter_array[i1,:] = self.order_parameter_array
+            
+        # Save data (probably not necessary anymore)
+        sim_times_final = np.mean(self.full_sim_times, axis = 0)
         print(sim_times_final)
-        x_probs = np.linspace(0, 1, np.size(sim_times_final)).reshape(
-            (self.probabilities, 1)
-        )
-        plt.plot(x_probs, np.transpose(sim_times_final), "k*")
-        plt.xlabel("Initial tree fraction")
-        plt.ylabel("Time until simulation complete")
-
+        if self.plot_option_raw == True: # If plotting raw data the figure and axes already exist
+            plt.xlabel("Initial tree fraction")
+            plt.ylabel("Time until simulation complete")
+        else:
+            # Make array of tree coverage fractions used
+            x_probs = np.linspace(0,1,np.size(sim_times_final)).reshape((self.probabilities,1))
+            # Plot mean
+            plt.figure(1)
+            plt.plot(x_probs,np.transpose(sim_times_final),'k*')
+            plt.xlabel("Initial tree fraction")
+            plt.ylabel("Mean time until simulation complete")
+            if self.plot_option_order == True:
+                # Plot order parameter on separate figure
+                order_parameter_final = np.mean(self.full_order_parameter_array, axis = 0)
+                plt.figure(2)
+                plt.plot(x_probs,np.transpose(order_parameter_final),'k*')
+                plt.xlabel("Initial tree fraction")
+                plt.ylabel("Order parameter")
+    
     def reset_outer_edges(self):
         """Resets the outer edges of the grid to zero"""
         self.grid[0 : self.grid_size] = 0
@@ -197,8 +219,8 @@ class ForestFire(BasePercolation):
             # Place trees randomly, with probabiliity from the loop in init_sim
             tree_fraction = self.iterative_tree_frac
             self.grid = (np.random.rand(self.grid.size) < tree_fraction).astype(np.int)
-            # Set 2nd row on fire
-            self.grid[self.grid_size : 2 * self.grid_size - 1] = 2
+            # Set 2nd row on fire 
+            self.grid[self.grid_size : 2*self.grid_size-1] = 2
             self.reset_outer_edges()
 
     def step(self):
@@ -253,9 +275,9 @@ class ForestFire(BasePercolation):
     def sim_step(self):
         # Modified version of step for criticality simulation. No tree growth,
         # no spontaneous fires, and fires only spread via nearest-neighbours
-        # (i.e. no North East, South West etc) so that results can be compared
+        # (i.e. no North East, South West etc) so that results can be compared 
         # with square site percolation
-        key_index = int(self.iterative_tree_frac * self.probabilities)
+        key_index = int(self.iterative_tree_frac*self.probabilities)
         self.test_grid = self.grid
         for i in range(self.grid.size - self.grid_size - 1):
             point = self.grid[i]
@@ -268,25 +290,37 @@ class ForestFire(BasePercolation):
                 self.grid[i + 1] = -2 if east else east
                 self.grid[i + self.grid_size] = -2 if south else south
                 self.grid[i - 1] = -2 if west else west
-
+                
                 self.grid[i] = 0
-
+                
         self.step_iteration += 1
         print(self.step_iteration)
         self.grid = np.absolute(self.grid)
         self.reset_outer_edges()
-        last_line = self.grid[
-            self.grid_size
-            ^ 2 - 2 * self.grid_size : self.grid_size
-            ^ 2 - self.grid_size - 1
-        ]
+        last_line = self.grid[self.grid_size^2-2*self.grid_size : self.grid_size^2-self.grid_size-1]
         # Algorithm complete if fire has spread to last line
         if 2 in last_line:
             self.is_simulating = False
-            self.sim_times[0, key_index] = self.step_iteration
+            self.sim_times[0,key_index] = self.step_iteration
             print("ALL THE WAY")
+            if self.plot_option_raw == True:
+                # Plot raw data
+                plt.plot(self.iterative_tree_frac,self.step_iteration,'k.')
+            if self.plot_option_order == True:
+                # Work out fraction of grid that's burning, remembering to 
+                # remove the outer edges
+                number_of_fires = np.count_nonzero(np.where(self.grid == 2,2,0))
+                self.order_parameter_array[0,key_index] = number_of_fires/((self.grid_size-2)^2)
         # Algorithm complete if grid doesn't change (fire can't reach new trees)
         if np.all(self.test_grid == self.grid):
             self.is_simulating = False
-            self.sim_times[0, key_index] = self.step_iteration
+            self.sim_times[0,key_index] = self.step_iteration
             print("BURNED ITSELF OUT")
+            if self.plot_option_raw == True:
+                #Plot raw data
+                plt.plot(self.iterative_tree_frac,self.step_iteration,'r.')
+            if self.plot_option_order == True:
+                # Work out fraction of grid that's burning, remembering to 
+                # remove the outer edges
+                number_of_fires = np.count_nonzero(np.where(self.grid == 2,2,0))
+                self.order_parameter_array[0,key_index] = number_of_fires/((self.grid_size-2)^2)
