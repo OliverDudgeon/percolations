764192b49c7a8b05dc801500e322914b4730a6ca
b0nagro <adamshaw5505@gmail.com>
2020-10-30 22:53:04 +0000

    Merged site_simulations into site_percolation

--- site_percolation.py
+++ site_percolation.py
@@ -8,10 +8,11 @@ import time
 import numpy as np
 import pygame
 import pygame_gui
-from pygame_gui.elements import UIButton, UIHorizontalSlider
+from pygame_gui.elements import UIButton, UIHorizontalSlider, UIDropDownMenu
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
 from scipy.ndimage import measurements
+from scipy.optimize import curve_fit
 
 from base_percolation import BasePercolation
 
@@ -69,9 +70,14 @@ class SitePercolation(BasePercolation):
         self.button_path = UIButton(
             pygame.Rect(620, 120, *BTN_WH), "Cluster", gui_manager
         )
-        self.button_graph = UIButton(
-            pygame.Rect(620, 180, *BTN_WH), "Plot", gui_manager
+
+        self.sim_select = UIDropDownMenu(
+            ["Fisher Exponent", "Beta Exponent", "Critical Point"],
+            "Fisher Exponent",
+            pygame.Rect((10, 710, 200, 30)),
+            gui_manager,
         )
+        self.button_sim = UIButton(pygame.Rect(10, 750, *BTN_WH), "Plot", gui_manager)
 
         self.step()  # Initial draw
 
@@ -131,8 +137,13 @@ class SitePercolation(BasePercolation):
             elif event.ui_element == self.button_path:
                 self.hoshen_kopelman()
                 self.draw_clusters = True
-            elif event.ui_element == self.button_graph:
-                self.simulate()
+            elif event.ui_element == self.button_sim:
+                if self.sim_select.selected_option == "Fisher Exponent":
+                    self._fisher_exponent()
+                elif self.sim_select.selected_option == "Beta Exponent":
+                    self._beta_exponent()
+                elif self.sim_select.selected_option == "Critical Point":
+                    self._sim_critical_point()
 
     def update(self, delta) -> None:
         return
@@ -160,54 +171,146 @@ class SitePercolation(BasePercolation):
             self.grid.reshape((self.grid_size, self.grid_size))
         )
         self.cluster = self.cluster.reshape(self.grid.size)
-        # """
-        # Implementation of the Hoshen-Kopelman clustering algorithm
-        # returns the number of clusters found
-        # """
-        # # Array of sites and what cluster they're in
-        # self.cluster = self.grid.astype(np.int)
-        # # Links clusters together that we're not previously found to be together
-        # self.labels[:] = 0
 
-        # for i in range(self.grid.size):
-        #     row = i % self.grid_size
-        #     column = i // self.grid_size
-        #     if self.grid[i]:  # Loops through all the active sites
-        #         # Cluster number of left site
-        #         top = 0 if row == 0 else self.cluster[i - 1]
-        #         # Cluster number of top site
-        #         left = 0 if column == 0 else self.cluster[i - self.grid_size]
-        #         # Counts how many sites are next to it
-        #         cluster = int(top > 0) + int(left > 0)
-        #         if cluster == 0:  # If not in cluster, add to new one
-        #             self.labels[0] += 1
-        #             self.labels[int(self.labels[0])] = self.labels[0]
-        #             self.cluster[i] = self.labels[0]
-        #         elif cluster == 1:
-        #             # Next to one cluster so set to the cluster number
-        #             # (one of left or top will be zero)
-        #             self.cluster[i] = max(top, left)
-        #         elif cluster == 2:  # Next to two clusters
-        #             self.cluster[i] = self._union(top, left)
+    def _fisher_exponent(self):
+        N = 40000
+        pc = 0.59274
+        grid = np.random.rand(N, N) < pc + 0.035
+        labels, _ = measurements.label(grid)
+        _, size = np.unique(labels, return_counts=True)
+        size, counts = np.unique(size, return_counts=True)
+        size = size[counts > N // 2]
+        counts = counts[counts > N // 2] / np.sum(counts)
 
-        # self.labels[self.labels[0]] = 0 # This is links emtyp cells to zero, makes them not drawn
-        # self.labels[0] = 0 # Stops infinite loop in find function
-        # self.cluster = self._find(self.cluster) # Give each cluster a unique label
+        def func(s, m, c):
+            return m * s + c
 
-    def _is_infinite(self) -> int:
-        """Check if the grid is infinite"""
-        # Find cluster numbers on top and bottom
-        top_bot = np.intersect1d(
-            self.cluster[: self.grid_size],
-            self.cluster[self.grid.size - self.grid_size :],
+        popt, pact = curve_fit(func, np.log(size), np.log(counts))
+        print(-popt[0], pact[0])
+        plt.plot(np.log(size), np.log(counts))
+        plt.plot(np.log(size), func(np.log(size), *popt))
+        plt.legend(
+            [
+                "Data plot",
+                r"$\log|n_{s}| \approx"
+                + f" {popt[0]:.3f}"
+                + r"\log|s|"
+                + f"{popt[1]:.2f}$",
+            ]
         )
-        # Find cluster numbers on left and right
-        left_right = np.intersect1d(
-            self.cluster[self.grid_size - 1 :: self.grid_size],
-            self.cluster[0 :: self.grid_size],
+        plt.title(
+            "Log-Log plot of cluster count vs the size of the cluster\n"
+            + r"$\tau = "
+            + f"{-popt[0]:.3f}"
+            + r"\pm"
+            + f"{pact[0][0]:.3e}$"
         )
-        # Remove empty sites
-        top_bot = top_bot[top_bot != 0]
-        left_right = left_right[left_right != 0]
-        # Returns 1 if a cluster belongs on opposite edges, 0 otherwise
-        return int(left_right.size > 0 or top_bot.size > 0)
+        plt.xlabel(r"$\log|s|$")
+        plt.ylabel(r"$\log|n_{s}|$")
+        plt.savefig("./site_data/tau_graph.pdf", format="pdf")
+        plt.show()
+
+    def _beta_exponent(self):
+        size = 20000
+        pc = 0.59274
+        prob_list = np.linspace(pc, 0.7, 300)
+        frac_list = np.zeros_like(prob_list)
+        try:
+            frac_list = np.load("./site_data/order_data.npy")
+        except FileNotFoundError:
+            for i in range(prob_list.size):
+                print(i)
+                grid = np.random.rand(size, size) < prob_list[i]
+                labels, _ = measurements.label(grid)
+                nlabel, count = np.unique(labels, return_counts=True)
+                top_bot = np.intersect1d(labels[:][0], labels[:][size - 1])
+                left_right = np.intersect1d(labels[0][:], labels[size - 1][:])
+                for j in np.union1d(top_bot, left_right)[1:]:
+                    frac_list[i] += count[np.where(nlabel == j)[0]] / (size * size)
+            np.save("./site_data/order_data.npy", frac_list)
+        prob_list -= pc
+        frac_list = frac_list[prob_list > 0]
+        prob_list = prob_list[prob_list > 0]
+
+        prob_list = prob_list[frac_list > 0]
+        frac_list = frac_list[frac_list > 0]
+        np.log(frac_list)
+
+        def func(x, m, c):
+            return m * x + c
+
+        popt, pact = curve_fit(func, np.log(prob_list), np.log(frac_list))
+        print(popt[0])
+        plt.plot(prob_list, frac_list)
+        plt.xlabel(r"|$p - p_{c}$|")
+        plt.ylabel(r"P")
+        plt.title(
+            r"Order parameter vs site probability for 2d site percolation, $P\sim|p-p_{c}|^{\beta}$"
+        )
+        plt.legend(["Data"])
+
+        plt.savefig("./site_data/order_fig.pdf", format="pdf")
+        plt.savefig("./site_data/order_fig.svg", format="svg")
+        plt.show()
+
+        plt.plot(np.log(prob_list), np.log(frac_list))
+        plt.plot(np.log(prob_list), func(np.log(prob_list), *popt))
+        plt.xlabel(r"$\log |p-p_{c}|$")
+        plt.ylabel(r"$\log P$")
+        plt.title(
+            f"Log-Log plot of order parameter vs site probability to find beta exponent $\\beta = {popt[0]}$"
+        )
+        plt.legend(["Data", "Linear Regression"])
+        plt.show()
+
+    def _sim_critical_point(self):
+        """To find critical point WIP"""
+        p_low = 0.0
+        p_high = 1.0
+        N = 40_000
+        ph_list = [1.0]
+        pl_list = [0.0]
+        try:
+            ph_list = np.load("./site_data/ph_list.npy")
+            pl_list = np.load("./site_data/pl_list.npy")
+        except FileNotFoundError:
+            while abs(p_high - p_low) > 0.0001:
+                p = (p_high + p_low) / 2
+                grid = np.random.rand(N, N) < p
+                labels, _ = measurements.label(grid)
+                inf_labels = np.union1d(
+                    np.intersect1d(labels[:][0], labels[:][N - 1]),
+                    np.intersect1d(labels[0][:], labels[N - 1][:]),
+                )
+                inf_labels = inf_labels[np.where(inf_labels != 0)]
+                if inf_labels.size > 0:
+                    p_high = p
+                else:
+                    p_low = p
+                print(p_low, p_high)
+                ph_list.append(p_high)
+                pl_list.append(p_low)
+            ph_list = np.asarray(ph_list)
+            pl_list = np.asarray(pl_list)
+            np.save("./site_data/ph_list.npy", ph_list)
+            np.save("./site_data/pl_list.npy", pl_list)
+        ns = np.arange(ph_list.size)
+        p_c = (ph_list[-1] + pl_list[-1]) / 2.0
+        pc_d = (ph_list[-1] - pl_list[-1]) / 2.0
+        plt.scatter(ns, ph_list)
+        plt.scatter(ns, pl_list)
+        plt.plot(ns, np.ones_like(ns) * p_c)
+        plt.legend(["Critical probability", "Upper Probability", "Lower Probability"])
+        plt.xlabel("Itteration number")
+        plt.ylabel("Probability")
+        pc_str = f"{p_c:.4f}"
+        pc_d_str = f"{pc_d:.1e}"
+        plt.title(
+            r"Plot of binary search for critical probability, $p_{c} ="
+            + pc_str
+            + r"\pm"
+            + pc_d_str
+            + "$"
+        )
+        plt.savefig("./site_data/critical_prob.pdf", format="pdf")
+        plt.show()
