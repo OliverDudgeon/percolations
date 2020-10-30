9cd47d7e00eadcc707412fb4392c5edd7f96ce84
b0nagro <adamshaw5505@gmail.com>
2020-10-30 15:58:02 +0000

    Created more results

--- site_simulations.py
+++ site_simulations.py
@@ -3,52 +3,21 @@
 import numpy as np
 from multiprocessing import Pool
 from scipy.ndimage import label
+from skimage import measure
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-
-def _gamma_exponent():
-    N = 10000
-    pc = 0.59274
-    prob_list = np.linspace(pc - 0.1, pc, 100)
-    cluster_mean = np.zeros_like(prob_list)
-    try:
-        cluster_mean = np.load("./site_data/clmean_data.npy")
-    except FileNotFoundError:
-        for i in range(prob_list.size):
-            print(i)
-            grid = np.random.rand(N, N) < prob_list[i]
-            labels, _ = label(grid)
-            _, count = np.unique(labels, return_counts=True)
-            size, n_s = np.unique(count, return_counts=True)
-            cluster_mean[i] = np.dot(size ** 2, n_s) / pc
-            # cluster_mean[i] = np.sum(grid) / s
-            # cluster_mean[i] = np.mean(count)
-        np.save("./site_data/clmean_data.npy", cluster_mean)
-    prob_list -= pc
-    prob_list = np.abs(prob_list)
-    # cluster_mean = cluster_mean[: prob_list.size]
-    # prob_list = prob_list[: prob_list.size]
-    # plt.plot(prob_list, cluster_mean)
-
-    def func(s, m, c):
-        return m * s + c
-
-    # popt, _ = curve_fit(func, np.log(prob_list), np.log(cluster_mean))
-    # print(-popt[0])
-    # plt.plot(np.log(prob_list), func(np.log(prob_list), *popt))
-    plt.plot(np.log(prob_list), np.log(cluster_mean))
-    plt.show()
+pc = 0.59274
 
 
 def _fisher_exponent():
-    N = 35000
-    grid = np.random.rand(N, N) < 0.59274
+    N = 40000
+    grid = np.random.rand(N, N) < pc
     labels, _ = label(grid)
     _, size = np.unique(labels, return_counts=True)
     size, counts = np.unique(size, return_counts=True)
-    size = size[counts > N // 4]
-    counts = counts[counts > N // 4] / np.sum(counts)
+    size = size[counts > 2 * N]
+    counts = counts[counts > 2 * N] / np.sum(counts)
 
     def func(s, m, c):
         return m * s + c
@@ -62,8 +31,7 @@ def _fisher_exponent():
 
 def _beta_exponent():
     size = 20000
-    pc = 0.59274
-    prob_list = np.linspace(0.59274, 0.7, 300)
+    prob_list = np.linspace(pc, 0.7, 300)
     frac_list = np.zeros_like(prob_list)
     try:
         frac_list = np.load("./site_data/order_data.npy")
@@ -114,7 +82,51 @@ def _beta_exponent():
     plt.show()
 
 
+def _sim_critical_point():
+    """To find critical point WIP"""
+    p_low = 0.0
+    p_high = 1.0
+    N = 40_000
+    ph_list = [1.0]
+    pl_list = [0.0]
+    try:
+        ph_list = np.load("./site_data/ph_list.npy")
+        pl_list = np.load("./site_data/pl_list.npy")
+    except FileNotFoundError:
+        while abs(p_high - p_low) > 0.0001:
+            p = (p_high + p_low) / 2
+            grid = np.random.rand(N, N) < p
+            labels, _ = label(grid)
+            inf_labels = np.union1d(
+                np.intersect1d(labels[:][0], labels[:][N - 1]),
+                np.intersect1d(labels[0][:], labels[N - 1][:]),
+            )
+            inf_labels = inf_labels[np.where(inf_labels != 0)]
+            if inf_labels.size > 0:
+                p_high = p
+            else:
+                p_low = p
+            print(p_low, p_high)
+            ph_list.append(p_high)
+            pl_list.append(p_low)
+        ph_list = np.asarray(ph_list)
+        pl_list = np.asarray(pl_list)
+        np.save("./site_data/ph_list.npy", ph_list)
+        np.save("./site_data/pl_list.npy", pl_list)
+    ns = np.arange(ph_list.size)
+    p_c = (ph_list[-1] + pl_list[-1]) / 2.0
+    plt.scatter(ns, ph_list)
+    plt.scatter(ns, pl_list)
+    plt.plot(ns, np.ones_like(ns) * p_c)
+    plt.legend(["Critical probability", "Upper Probability", "Lower Probability"])
+    plt.xlabel("Itteration number")
+    plt.ylabel("Probability")
+    plt.title(r"Plot of binary search for critical probability, $p_{c} = 0.5928$")
+    plt.savefig("./site_data/critical_prob.pdf", format="pdf")
+    plt.show()
+
+
 if __name__ == "__main__":
-    _gamma_exponent()
+    _sim_critical_point()
     # _fisher_exponent()
     # _beta_exponent()
